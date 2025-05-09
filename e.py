bf8c4d06c2d95287885f0b5f8c0409c850ddc8c4
shivam-chaturvedi <126079471+shivam-chaturvedi@users.noreply.github.com>
2025-05-09 16:37:56 +0530

    added multi question in one page bypass

--- e.py
+++ e.py
@@ -1,4 +1,3 @@
-# !pip install together
 # !pip install cohere
 # !pip install webdriver-manager
 
@@ -15,13 +14,22 @@ from google import genai
 import sys
 import concurrent.futures
 
-print("Script name:", sys.argv[0])
-print("Arguments:", sys.argv[1:])
 
-link = str(sys.argv[1])
+try:
+    print("Script name:", sys.argv[0])
+    print("Arguments:", sys.argv[1:])
+
+    link = str(sys.argv[1])
+
+    if(link is None):
+        print("Link Cant Be Empty!")
+        sys.exit(0)
+except Exception as e:
+    print("Link Cant Be Empty!")
+    sys.exit(0)
 
 # Gemini function
-def get_answer_from_gemini(question, options):
+def get_answer_from_gemini_normal(question, options):
     prompt = f'''Answer the following question and choose one option:
     Question: {question}
     Options:
@@ -33,9 +41,8 @@ def get_answer_from_gemini(question, options):
         response = client.models.generate_content(
             model="gemini-2.0-flash",
             contents=prompt + ''' 
-            ans this and give answer only in one word only goive ANSWER IN OPTIONS LIKE A,B,C,D,E ETC NOT PRINT ANYTHING ELSE
-     USE WEB SEARCH AND SEARCH ON GOOGLE FOR ANSWERS AND GENERATE ANSWER CORRECTLY            
-
+            ans this and give answer only in one word only give ANSWER IN OPTIONS LIKE A,B,C,D,E ETC NOT PRINT ANYTHING ELSE
+            USE WEB SEARCH AND SEARCH ON GOOGLE FOR ANSWERS AND GENERATE ANSWER CORRECTLY
             ''',
         )
         print(response.text)
@@ -57,9 +64,8 @@ def get_answer_from_gemini_pro(question, options):
     response = client.models.generate_content(
         model="gemini-2.5-pro-preview-05-06",
         contents=prompt+'''
-            ans this and give answer only in one word only goive ANSWER IN OPTIONS LIKE A,B,C,D,E ETC NOT PRINT ANYTHING ELSE
-     USE WEB SEARCH AND SEARCH ON GOOGLE FOR ANSWERS AND GENERATE ANSWER CORRECTLY            
-
+            ans this and give answer only in one word only give ANSWER IN OPTIONS LIKE A,B,C,D,E ETC NOT PRINT ANYTHING ELSE
+            USE WEB SEARCH AND SEARCH ON GOOGLE FOR ANSWERS AND GENERATE ANSWER CORRECTLY
         ''',
     )
 
@@ -67,20 +73,36 @@ def get_answer_from_gemini_pro(question, options):
     return response.text
 
 
-# Smart selector with timeout
-def get_ans(question, options):
-    print("\nSelecting answer for:\n", question)
+def get_answer_from_gemini_flash(question, options):
+    prompt = f'''Answer the following question and choose one option:
+    Question: {question}
+    Options:
+    {options}
+    Answer (Choose one option):'''
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future = executor.submit(get_answer_from_gemini_pro, question, options)
-        try:
-            answer = future.result(timeout=30)  # wait max 20 seconds
-            if answer:
-                return answer
-        except concurrent.futures.TimeoutError:
-            print("Gemini Pro took too long. Falling back to normal Gemini...")
+    client = genai.Client(api_key="AIzaSyARkc9tT3MYUZw6lfIVZ1_EZR-sdSYJF1w")
 
-    return get_answer_from_gemini(question, options)
+    response = client.models.generate_content(
+        model="models/gemini-2.5-flash-preview-04-17",
+        contents=prompt+'''
+            ans this and give answer only in one word only give ANSWER IN OPTIONS LIKE A,B,C,D,E ETC NOT PRINT ANYTHING ELSE
+            USE WEB SEARCH AND SEARCH ON GOOGLE FOR ANSWERS AND GENERATE ANSWER CORRECTLY
+        ''',
+    )
+
+    print(response.text)
+    return response.text
+
+
+def get_ans(question, options, model):
+    if(model=="p"):        
+        print("Getting from pro")
+        return get_answer_from_gemini_pro(question, options)
+    elif(model=="f"):
+        print("Getting from flash")
+        return get_answer_from_gemini_flash(question, options)
+    return get_answer_from_gemini_normal(question,options)
+
 
 # Setup browser with auto-downloaded Edge driver
 options = Options()
@@ -91,18 +113,34 @@ driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install(
 # Open Moodle and wait for user login
 driver.get(link)
 WebDriverWait(driver, 3000).until(EC.presence_of_element_located((By.ID, "username")))
-
-print("Please enter username and password manually.")
-time.sleep(6)  # Give user time to enter credentials
-driver.find_element(By.ID, "loginbtn").click()
-print("Logged in successfully.")
-
+n=0
 # Quiz loop
 while True:
     try:
         WebDriverWait(driver, 3000).until(EC.presence_of_element_located((By.CLASS_NAME, "qtext")))
-        question_text = driver.find_element(By.CLASS_NAME, "qtext").text.strip()
-        options_div = driver.find_element(By.CLASS_NAME, "ablock")
+  # Get all questions on the page
+        questions = driver.find_elements(By.CLASS_NAME, "qtext")
+        answers= driver.find_elements(By.CLASS_NAME, "ablock")
+        # print(questions)
+        if not questions:
+            print("No more questions found!")
+            break
+
+        # # Focus on the question specified by the user
+        current_question = questions[n]
+        current_answer=answers[n]
+
+        print("TOTAL NUMBE ROF QUESTIONS ",len(questions))
+
+        if(len(questions)>1):
+            question_text=current_question.text.strip()
+            
+            options_div = current_answer
+        else:
+            question_text = driver.find_element(By.CLASS_NAME, "qtext").text.strip()
+                
+            options_div = driver.find_element(By.CLASS_NAME, "ablock")
+            
         option_labels = options_div.find_elements(By.TAG_NAME, "label")
         options_dict = {}
 
@@ -120,8 +158,26 @@ while True:
         print("\nOptions:")
         for k, v in options_dict.items():
             print(f"{k} {v}")
+        
+        model = "n"
 
-        selected_option = get_ans(question_text, options_text).strip().lower()
+        # Take input from different keys and print the selected model
+        if keyboard.is_pressed('q'):
+            model = "p"
+            print("You chose pro model")
+        elif keyboard.is_pressed('z'):
+            model = "f"
+            print("You chose flash model")
+        elif keyboard.is_pressed('s'):                                          
+            model = "n"
+            print("Falling back to normal model")
+        elif keyboard.is_pressed("r"):
+            print("Resetting")
+            n=0
+        
+
+        
+        selected_option = get_ans(question_text, options_text, model).strip().lower()
         print(f"\nSelected option: {selected_option}")
         selected_option = list(selected_option)[0]
 
@@ -136,13 +192,13 @@ while True:
             except Exception as e:
                 print(f"Error matching label: {e}")
                 continue
-
-        print("\nWaiting for Ctrl+Alt+A to continue to next question...")
-        keyboard.wait('ctrl+alt+a')
-        print("Proceeding to next...\n")
+        
+        keyboard.wait("alt")  # Wait for the next key press
+        n+=1
+        if(n>len(questions)-1):
+            n=0
 
     except Exception as e:
-        print("Error:", e)
         break
 
 driver.quit()
